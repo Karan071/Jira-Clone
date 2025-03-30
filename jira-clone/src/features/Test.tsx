a780af8c4045b74bafefa188461ef42eaa6558ed
Karan Chourasia <70134825+Karan071@users.noreply.github.com>
2025-03-30 18:47:12 +0000

    customizing components

--- jira-clone/src/features/Test.tsx
+++ jira-clone/src/features/Test.tsx
@@ -1,3 +1,17 @@
+import { Button } from "@/components/ui/button";
+
 export default function Test() {
-  return <div>Test Component</div>;
+  return (
+    <div>
+      <div className="flex gap-4 p-5">
+        <Button variant="primary" size="xs">Primary</Button>
+        <Button variant="destructive">destructive</Button>
+        <Button variant="ghost">ghost</Button>
+        <Button variant="muted">muted</Button>
+        <Button variant="outline">outline</Button>
+        <Button variant="secondary">secondary</Button>
+        <Button variant="teritary">teritrary</Button>
+      </div>
+    </div>
+  );
 }
